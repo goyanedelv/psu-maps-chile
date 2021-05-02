8d2ad99190a56032d410f5eb30b4b5301e48d7c7
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-05-01 23:14:56 -0500

    Update leafletmap.R

--- leafletmap.R
+++ leafletmap.R
@@ -1,8 +1,6 @@
 
-#Mapas con leaftet
+# Map with leadlet
 
-setwd("C:/Users/Gonzalo/Desktop/goyanedelv.github.io")
-#install.packages("leaflet")
 library(leaflet)
 library(htmlwidgets)
 
@@ -13,23 +11,11 @@ map = readOGR(geosonmap, "OGRGeoJSON")
 map$NOM_COM <- as.character(map$NOM_COM) 
 Encoding(map$NOM_COM) <- "latin1" 
 
-datos=read.csv("C:/Users/Gonzalo/Dropbox/Super Data Projects/Chilean Data Project/Map Function R/plantilla_static_maps1756.csv")
-datos$Cook[datos$Cook>12]=12 #Limitar Cook Index a máximo 12
+datos=read.csv("plantilla_static_maps1756.csv")
 map@data=data.frame(map@data,datos[match(map@data[,"COD_COMUNA"],datos[,"COD_COMUNA"]),])
 map@data$ï..comunas=NULL
 
-pal <- colorNumeric("RdBu", NULL)
 pal2 <- colorNumeric("Spectral",NULL)
-
-m<-leaflet(map) %>%
-	addTiles() %>%
-	addProviderTiles(providers$OpenStreetMap) %>%
-	#addMarkers(lng=-70.64827, lat=-33.45694, popup="Aki le pedi pololeo a la oli <3")%>%
-	addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.8,
-    	fillColor = ~pal(map$Cook),
-    	label = ~paste0(map$NOM_COM, ": ", formatC(map$Cook, big.mark = ","))) %>%
-    addLegend(pal = pal, values = map$Cook, opacity = 1.0)#,
-saveWidget(m, file="index.html") #guarda como html
 
 m<-leaflet(map) %>%
 	addTiles() %>%
@@ -37,7 +23,5 @@ m<-leaflet(map) %>%
 	addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.8,
     	fillColor = ~pal2(map$psupond),
     	label = ~paste0(map$NOM_COM, ": ", formatC(map$psupond, big.mark = ","))) %>%
-    addLegend(pal = pal2, values = map$psupond, opacity = 1.0)#,
-saveWidget(m, file="index.html") #guarda como html
-
-#Multiple layers: https://statnmap.com/en/2016/12/multiple-layers-leaflet-widget-with-rstat/
+    addLegend(pal = pal2, values = map$psupond, opacity = 1.0)
+saveWidget(m, file="index.html") # save as html
